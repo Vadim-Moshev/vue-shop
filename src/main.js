0d851289336778c91799fc20179e96ba411780d5
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-04-15 00:38:55 +0500

    add vuestic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(createVuestic()).mount('#app')
