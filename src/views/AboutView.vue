23f21798705a053030ca8063fd0720384cfd6357
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-04-15 01:17:50 +0500

    some test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <h1 class="va-h1">Display 1 Heading</h1>
+  <va-button loading icon="clear" icon-right="unfold_more" class="mb-2" />
 </template>
