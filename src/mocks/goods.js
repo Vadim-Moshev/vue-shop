0d22a22175bc14ff34602924136f8b619ff5802e
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-04-26 00:05:10 +0500

    restore missing commas

--- src/mocks/goods.js
+++ src/mocks/goods.js
@@ -1,49 +1,52 @@
-/* все webp конвертировать в jpg или png и регистр имени файла на малый поменять*/
-
 export default [
   {
-   id: "1-1",
-   category: "monitors"
-   title: "21.45 Монитор DEXP DF22N1 серебристый",
-   photo: "dexp-df22n1.jpg",
-   description: "1920x1080 (FullHD)@75 Гц, VA, 3000 : 1, 250 Кд/м², 170°/170°, HDMI 1.4, VGA (D-Sub), AMD FreeSync",
-   price: "6500",
+    id: "1-1",
+    category: "monitors",
+    title: "21.45 Монитор DEXP DF22N1 серебристый",
+    photo: "dexp-df22n1.jpg",
+    description:
+      "1920x1080 (FullHD)@75 Гц, VA, 3000 : 1, 250 Кд/м², 170°/170°, HDMI 1.4, VGA (D-Sub), AMD FreeSync",
+    price: "6500",
   },
   {
     id: "1-2",
-    category: "monitors"
+    category: "monitors",
     title: "19.5 Монитор Acer V206HQLAb черный",
     photo: "acer-v206hqlab.jpg",
-    description: "1600x900@60 Гц, TN, 5 мс, 600 : 1, 200 Кд/м², 90°/65°, VGA (D-Sub)",
+    description:
+      "1600x900@60 Гц, TN, 5 мс, 600 : 1, 200 Кд/м², 90°/65°, VGA (D-Sub)",
     price: "7500",
   },
   {
     id: "1-3",
-    category: "monitors"
+    category: "monitors",
     title: "Монитор AOC E970SWN черный",
     photo: "aoc-e970swn.jpg",
-    description: "1366x768@60 Гц, TN, 5 мс, 700 : 1, 200 Кд/м², 90°/65°, VGA (D-Sub)",
+    description:
+      "1366x768@60 Гц, TN, 5 мс, 700 : 1, 200 Кд/м², 90°/65°, VGA (D-Sub)",
     price: "7600",
   },
   {
     id: "1-4",
-    category: "monitors"
+    category: "monitors",
     title: "Монитор Philips 203V5LSB26 черный",
     photo: "philips-203v5lsb26.jpg",
-    description: "1600x900@60 Гц, TN, 5 мс, 600 : 1, 200 Кд/м², 90°/50°, VGA (D-Sub)",
+    description:
+      "1600x900@60 Гц, TN, 5 мс, 600 : 1, 200 Кд/м², 90°/50°, VGA (D-Sub)",
     price: "7700",
   },
   {
     id: "1-5",
-    category: "monitors"
+    category: "monitors",
     title: "21.5 Монитор Philips 221V8 черный",
     photo: "philips-221v8.jpg",
-    description: "1920x1080 (FullHD)@75 Гц, VA, 3000 : 1, 250 Кд/м², 178°/178°, HDMI 1.4, VGA (D-Sub), AMD FreeSync",
+    description:
+      "1920x1080 (FullHD)@75 Гц, VA, 3000 : 1, 250 Кд/м², 178°/178°, HDMI 1.4, VGA (D-Sub), AMD FreeSync",
     price: "7700",
   },
   {
     id: "2-1",
-    category: "mice"
+    category: "mice",
     title: "Мышь проводная Aceline CM-408BU черный",
     photo: "aceline-cm-408bu.jpg",
     description: "800 dpi, светодиодный, USB Type-A, кнопки - 3",
@@ -51,7 +54,7 @@ export default [
   },
   {
     id: "2-2",
-    category: "mice"
+    category: "mice",
     title: "Мышь беспроводная DEXP WM-4005GU серый",
     photo: "dexp-wm-4005gu.jpg",
     description: "1600 dpi, светодиодный, USB Type-A, кнопки - 4",
@@ -59,7 +62,7 @@ export default [
   },
   {
     id: "2-3",
-    category: "mice"
+    category: "mice",
     title: "Мышь проводная DEXP Notos черный",
     photo: "dexp-notos.jpg",
     description: "2400 dpi, светодиодный, USB Type-A, кнопки - 6",
@@ -67,7 +70,7 @@ export default [
   },
   {
     id: "3-1",
-    category: "keyboards"
+    category: "keyboards",
     title: "Клавиатура проводная Defender Spark GK-300L [45300]",
     photo: "defender-spark-gk-300l.jpg",
     description: "мембранная, клавиш - 104, USB, черная",
@@ -75,7 +78,7 @@ export default [
   },
   {
     id: "3-2",
-    category: "keyboards"
+    category: "keyboards",
     title: "Клавиатура проводная Aceline K-505BU",
     photo: "aceline-k-505bu.jpg",
     description: "мембранная, клавиш - 112, USB, черная",
@@ -83,7 +86,7 @@ export default [
   },
   {
     id: "3-3",
-    category: "keyboards"
+    category: "keyboards",
     title: "Клавиатура проводная ExeGate LY-401 [EX264086RUS]",
     photo: "exegate-ly-401.jpg",
     description: "мембранная, клавиш - 104, USB, серебристая",
@@ -91,7 +94,7 @@ export default [
   },
   {
     id: "3-4",
-    category: "keyboards"
+    category: "keyboards",
     title: "Клавиатура проводная DEXP Sparkle",
     photo: "dexp-sparkle.jpg",
     description: "мембранная, клавиш - 112, USB, черная",
@@ -99,7 +102,7 @@ export default [
   },
   {
     id: "4-1",
-    category: "hdd"
+    category: "hdd",
     title: "1 ТБ Жесткий диск Seagate SkyHawk [ST1000VX005]",
     photo: "seagate-skyhawk.jpg",
     description: "SATA III, 6 Гбит/с, 5900 об/мин, кэш память - 64 МБ",
@@ -107,7 +110,7 @@ export default [
   },
   {
     id: "4-2",
-    category: "hdd"
+    category: "hdd",
     title: "1 ТБ Жесткий диск Toshiba DT01 [DT01ACA100]",
     photo: "toshiba-dt01.jpg",
     description: "SATA III, 6 Гбит/с, 7200 об/мин, кэш память - 32 МБ",
@@ -115,7 +118,7 @@ export default [
   },
   {
     id: "4-3",
-    category: "hdd"
+    category: "hdd",
     title: "1 ТБ Жесткий диск Seagate BarraCuda [ST1000DM010]",
     photo: "seagate-barracuda.jpg",
     description: "SATA III, 6 Гбит/с, 7200 об/мин, кэш память - 64 МБ",
@@ -123,7 +126,7 @@ export default [
   },
   {
     id: "4-4",
-    category: "hdd"
+    category: "hdd",
     title: "1 ТБ Жесткий диск WD Blue [WD10EZEX]",
     photo: "wd-blue.jpg",
     description: "SATA III, 6 Гбит/с, 7200 об/мин, кэш память - 64 МБ",
@@ -131,7 +134,7 @@ export default [
   },
   {
     id: "4-5",
-    category: "hdd"
+    category: "hdd",
     title: "1 ТБ Жесткий диск Toshiba P300 [HDWD110UZSVA]",
     photo: "toshiba-p300.jpg",
     description: "SATA III, 6 Гбит/с, 7200 об/мин, кэш память - 64 МБ",
@@ -139,106 +142,120 @@ export default [
   },
   {
     id: "5-1",
-    category: "ssd"
+    category: "ssd",
     title: "120 ГБ 2.5 SATA накопитель Apacer AS340 PANTHER [AP120GAS340G-1]",
     photo: "apacer-as340-panther.jpg",
-    description: "SATA, чтение - 550 Мбайт/сек, запись - 520 Мбайт/сек, 3D NAND 3 бит TLC",
+    description:
+      "SATA, чтение - 550 Мбайт/сек, запись - 520 Мбайт/сек, 3D NAND 3 бит TLC",
     price: "1100",
   },
   {
     id: "5-2",
-    category: "ssd"
+    category: "ssd",
     title: "120 ГБ 2.5 SATA накопитель AMD Radeon R5 Series [R5SL120G]",
     photo: "amd-radeon-r5-series.jpg",
-    description: "SATA, чтение - 520 Мбайт/сек, запись - 290 Мбайт/сек, 3D NAND 3 бит TLC",
+    description:
+      "SATA, чтение - 520 Мбайт/сек, запись - 290 Мбайт/сек, 3D NAND 3 бит TLC",
     price: "950",
   },
   {
     id: "5-3",
-    category: "ssd"
+    category: "ssd",
     title: "120 ГБ 2.5 SATA накопитель Apacer AS340X [AP120GAS340XC-1]",
     photo: "apacer-as340x.jpg",
-    description: "SATA, чтение - 550 Мбайт/сек, запись - 500 Мбайт/сек, 3D NAND 3 бит TLC",
+    description:
+      "SATA, чтение - 550 Мбайт/сек, запись - 500 Мбайт/сек, 3D NAND 3 бит TLC",
     price: "999",
   },
   {
     id: "5-4",
-    category: "ssd"
+    category: "ssd",
     title: "120 ГБ 2.5 SATA накопитель Smartbuy Nova [SBSSD120-NOV-25S3]",
     photo: "smartbuy-nova.jpg",
-    description: "SATA, чтение - 520 Мбайт/сек, запись - 490 Мбайт/сек, 3D NAND 3 бит TLC",
+    description:
+      "SATA, чтение - 520 Мбайт/сек, запись - 490 Мбайт/сек, 3D NAND 3 бит TLC",
     price: "999",
   },
   {
     id: "5-5",
-    category: "ssd"
-    title: "128 ГБ 2.5 SATA накопитель Neo Forza Zion NFS01 [NFS011SA328-6007200]",
+    category: "ssd",
+    title:
+      "128 ГБ 2.5 SATA накопитель Neo Forza Zion NFS01 [NFS011SA328-6007200]",
     photo: "neo-forza-zion-nfs01.jpg",
-    description: "SATA, чтение - 560 Мбайт/сек, запись - 490 Мбайт/сек, 3D NAND 3 бит TLC",
+    description:
+      "SATA, чтение - 560 Мбайт/сек, запись - 490 Мбайт/сек, 3D NAND 3 бит TLC",
     price: "999",
   },
   {
     id: "6-1",
-    category: "laptops"
+    category: "laptops",
     title: "15.6 Ноутбук Lenovo V15 ADA серый",
     photo: "lenovo-v15-ada.jpg",
-    description: "Full HD (1920x1080), TN+film, AMD 3020e, ядра: 2 х 1.2 ГГц, RAM 4 ГБ, SSD 128 ГБ, AMD Radeon Graphics , без ОС",
+    description:
+      "Full HD (1920x1080), TN+film, AMD 3020e, ядра: 2 х 1.2 ГГц, RAM 4 ГБ, SSD 128 ГБ, AMD Radeon Graphics , без ОС",
     price: "23899",
   },
   {
     id: "6-2",
-    category: "laptops"
+    category: "laptops",
     title: "14 Ноутбук Irbis NB257 серый",
     photo: "irbis-nb257.jpg",
-    description: "HD (1366x768), IPS, Intel Celeron 3350, ядра: 2 х 1.1 ГГц, RAM 4 ГБ, eMMC 64 ГБ, Intel HD Graphics 500 , Windows 10 Home Single Language",
+    description:
+      "HD (1366x768), IPS, Intel Celeron 3350, ядра: 2 х 1.1 ГГц, RAM 4 ГБ, eMMC 64 ГБ, Intel HD Graphics 500 , Windows 10 Home Single Language",
     price: "15999",
   },
   {
     id: "6-3",
-    category: "laptops"
+    category: "laptops",
     title: "14 Ноутбук DEXP Aquilon серебристый",
     photo: "dexp-aquilon.jpg",
-    description: "Full HD (1920x1080), IPS, Intel Celeron N4020, ядра: 2 х 1.1 ГГц, RAM 4 ГБ, SSD 128 ГБ, Intel UHD Graphics 600 , Windows 11 Pro",
+    description:
+      "Full HD (1920x1080), IPS, Intel Celeron N4020, ядра: 2 х 1.1 ГГц, RAM 4 ГБ, SSD 128 ГБ, Intel UHD Graphics 600 , Windows 11 Pro",
     price: "14299",
   },
   {
     id: "6-4",
-    category: "laptops"
+    category: "laptops",
     title: "15.6 Ноутбук ASUS Laptop 15 D543MA-DM1368 черный",
     photo: "asus-laptop-15-d543ma-dm1368.jpg",
-    description: "Full HD (1920x1080), TN+film, Intel Celeron N4020, ядра: 2 х 1.1 ГГц, RAM 4 ГБ, HDD 1000 ГБ, Intel UHD Graphics , без ОС",
+    description:
+      "Full HD (1920x1080), TN+film, Intel Celeron N4020, ядра: 2 х 1.1 ГГц, RAM 4 ГБ, HDD 1000 ГБ, Intel UHD Graphics , без ОС",
     price: "19999",
   },
   {
     id: "7-1",
-    category: "printers"
+    category: "printers",
     title: "Принтер лазерный Pantum P2500W",
     photo: "pantum-p2500w.jpg",
-    description: "черно-белая печать, A4, 1200x1200 dpi, ч/б - 22 стр/мин (A4), USB, Wi-Fi",
+    description:
+      "черно-белая печать, A4, 1200x1200 dpi, ч/б - 22 стр/мин (A4), USB, Wi-Fi",
     price: "9500",
   },
   {
     id: "7-2",
-    category: "printers"
+    category: "printers",
     title: "Принтер лазерный Pantum P2502",
     photo: "pantum-p2502.jpg",
-    description: "черно-белая печать, A4, 1200x1200 dpi, ч/б - 22 стр/мин (A4), USB 2.0",
+    description:
+      "черно-белая печать, A4, 1200x1200 dpi, ч/б - 22 стр/мин (A4), USB 2.0",
     price: "8500",
   },
   {
     id: "7-3",
-    category: "printers"
+    category: "printers",
     title: "Принтер лазерный Pantum P2200",
     photo: "pantum-p2200.jpg",
-    description: "черно-белая печать, A4, 1200x1200 dpi, ч/б - 20 стр/мин (A4), USB",
+    description:
+      "черно-белая печать, A4, 1200x1200 dpi, ч/б - 20 стр/мин (A4), USB",
     price: "9000",
   },
   {
     id: "7-4",
-    category: "printers"
+    category: "printers",
     title: "Принтер лазерный Pantum P2518",
     photo: "pantum-p2518.jpg",
-    description: "черно-белая печать, A4, 600x600 dpi, ч/б - 22 стр/мин (A4), USB 2.0",
+    description:
+      "черно-белая печать, A4, 600x600 dpi, ч/б - 22 стр/мин (A4), USB 2.0",
     price: "9000",
   },
-]
+];
