e0e69c867df6d37f5a3f5362e2dfda6868eda0bb
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-04-26 00:35:09 +0500

    add content for main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,20 @@
-<template>Я супер специалист во VUE!</template>
+<template>
+  <h1 class="page-header">Интернет-магазин компьютерной техники</h1>
+  <p>
+    Добро пожаловать в интернет-магазин компьютерной техники. Выберите
+    интересующую вас категорию в меню слева.
+  </p>
+</template>
 
 <script>
 export default {
   name: "MainPage",
 };
 </script>
+
+<style scoped>
+.page-header {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+</style>
