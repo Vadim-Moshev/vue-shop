9cbea74098dfc75dee5545e3a9fc5222bf5ca44d
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-04-26 02:20:59 +0500

    add switching functionality to categoryViewer

--- src/views/CategoryViewer.vue
+++ src/views/CategoryViewer.vue
@@ -2,18 +2,35 @@
   <div class="wrapper">
     <h2>Вы смотрите категорию {{ this.$route.params.categoryName }}</h2>
     <div class="goods-container">
-      <GoodItem goodName="Пётр" />
+      <GoodItem v-for="item in goods" :key="item.id" :goodName="item.title" />
     </div>
   </div>
 </template>
 
 <script>
 import GoodItem from "@/components/GoodItem";
+import goods from "@/mocks/goods";
 
 export default {
   name: "CategoryViewer",
   components: {
     GoodItem,
+  },
+  date() {
+    return {
+      goods: [],
+    };
+  },
+  created() {
+    this.goods = goods.filter((item) => {
+      return item.category === this.$route.params.categoryName;
+    });
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.goods = goods.filter((item) => {
+      return item.category === to.params.categoryName;
+    });
+    next();
   },
 };
 </script>
