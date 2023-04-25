6273534a1faadc15517c537a45b5837d2261b396
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-04-26 02:41:20 +0500

    add caterories indices

---
+++ src/helpers/categories-indices.js
@@ -0,0 +1,7 @@
+export const CAT_MONITORS = 0;
+export const CAT_MICE = 1;
+export const CAT_KEYBOARDS = 2;
+export const CAT_HDD = 3;
+export const CAT_SSD = 4;
+export const CAT_LAPTOPS = 5;
+export const CAT_PRINTERS = 6;
