06b866bc76edb244e312c5ad7ad1dbdccb9fee8f
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-04-27 23:42:37 +0500

    create ShopCartLink Component

---
+++ src/components/ShopCartLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link to="/shopcart" class="cart-link">{{ Somecaption }}</router-link>
+</template>
+
+<script>
+export default {
+  name: "ShopCartLink",
+  props: {
+    Somecaption: String,
+  },
+};
+</script>
+
+<style scoped>
+.cart-link {
+  color: #fff;
+  font-size: 16px;
+}
+
+.logo:hover,
+.cart-link:hover {
+  color: darkorange;
+}
+</style>
