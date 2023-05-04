cdc80abcdb63fa0cfabb5e8f4212b90c3e1ef95e
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-05-04 19:36:16 +0500

    create form on makeOrder page

--- src/views/MakeOrder.vue
+++ src/views/MakeOrder.vue
@@ -1,8 +1,37 @@
 <template>
-  <div>
+  <div class="make-order__header-container">
     <h1 v-if="cartIsNotEmpy">Оформление заказа</h1>
     <h1 v-else>оформление зказа недоступно, поскольку ваша корзина пуста</h1>
   </div>
+  <form v-if="cartIsNotEmpy" class="make-order__form" action="" novalidate>
+    <div class="make-order__form-row">
+      <span class="make-order__field-caption">
+        Ваше имя <i class="required"></i>
+      </span>
+      <input class="make-order__field" type="text" v-model="name" />
+    </div>
+    <div class="make-order__form-row">
+      <span class="make-order__field-caption">
+        Ваш адрес <i class="required"></i
+      ></span>
+      <input class="make-order__field" type="text" v-model="address" />
+    </div>
+    <div class="make-order__form-row">
+      <span class="make-order__field-caption">
+        Ваш телефон <i class="required"></i
+      ></span>
+      <input class="make-order__field" type="text" v-model="phone" />
+    </div>
+    <div class="make-order__form-row">
+      <span class="make-order__field-caption"> Комментарий к заказу </span>
+      <input class="make-order__field" type="text" v-model="comment" />
+    </div>
+    <span class="make-order__notice"
+      >Если все поля былы заполнены верно, то при нажатии на кнопку «Оформить
+      заказ» все товары из корзины будут удалены.</span
+    >
+    <input class="make-order__submit" type="submit" />
+  </form>
 </template>
 
 <script>
@@ -10,6 +39,15 @@ import boughtGoodsStorage from "@/mocks/bought-goods-storage";
 
 export default {
   name: "MakeOrder",
+  data() {
+    return {
+      name: "",
+      address: "",
+      phone: "",
+      comment: "",
+      isError: false,
+    };
+  },
   computed: {
     cartIsNotEmpy() {
       return boughtGoodsStorage.cartSize > 0;
@@ -18,4 +56,65 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.make-order__header-container {
+  padding: 15px;
+  padding-left: 0;
+  border-bottom: 2px solid navy;
+}
+
+.make-order__form {
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.make-order__form-row {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.make-order__field-caption {
+  width: 120px;
+}
+
+.required {
+  position: relative;
+}
+
+.required:after {
+  content: "*";
+  position: absolute;
+  color: #f00;
+  font-size: 22px;
+}
+
+.make-order__field {
+  flex-grow: 1;
+  padding: 5px;
+  border-radius: 5px;
+  border: 1px solid silver;
+}
+
+.make-order__field:focus {
+  border-color: navy;
+}
+
+.make-order__notice {
+  color: navy;
+  font-weight: bold;
+}
+
+.make-order__submit {
+  align-self: flex-start;
+  padding: 10px;
+  background-color: navy;
+  border: none;
+  color: #fff;
+  font-weight: bold;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
