2d1d81ecb6c007b3ca679cbadcf78845119ecfea
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2023-05-05 13:48:56 +0500

    remove vuestic and install again

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { createVuestic } from 'vuestic-ui'
 import 'vuestic-ui/css'
+import 'vuestic-ui/css'
 
 createApp(App).use(router).use(createVuestic()).mount('#app')
